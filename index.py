21381316fdef6b39dccc68782fd33df029e6763c
Heyyaditya <adityad2908@gmail.com>
2026-01-06 12:26:12 +0530

    index modified

--- index.py
+++ index.py
@@ -84,5 +84,7 @@ JOB DESCRIPTION:
                 "status": "fail",
                 "desc": str(e)
             }]
-        }), 500
+        })
  
+if __name__ == "__main__":
+    app.run(debug=True)
